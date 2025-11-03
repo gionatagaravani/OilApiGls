1be57259bb26dcad0e7000f22d89d22b962f8da9
Gionata Garavani <gionatagaravani@libero.it>
2025-11-04 00:00:29 +0100

    feat: post api

--- Controllers/OilPriceTrendController.cs
+++ Controllers/OilPriceTrendController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using OilTrendApi.Models;
+using OilTrendApi.Services;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +8,34 @@ using System.Linq;
 [Route("api/[controller]")]
 public class OilPriceTrendController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<IEnumerable> GetAll() => Ok([]);
+    private readonly IBrentService _brentService;
+    public OilPriceTrendController(IBrentService brentService)
+    {
+        _brentService = brentService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] OilPriceRequest request)
+    {
+        if (request == null)
+        {
+            return BadRequest();
+        }
+
+        var startDate = DateTime.TryParse(request.Params.StartDateISO8601, out var start) ? start : (DateTime?)null;
+        var endDate = DateTime.TryParse(request.Params.EndDateISO8601, out var end) ? end : (DateTime?)null;
+
+        if (!startDate.HasValue || !endDate.HasValue)
+        {
+            return BadRequest("Invalid date format. Use ISO 8601 format (ex: '2023-01-01')");
+        }
+        var prices = await _brentService.GetDailyPricesAsync(startDate.Value, endDate.Value);
+        
+        return Ok(new JsonRpcResponse
+        {
+            Jsonrpc = "2.0",
+            Id = request.Id,
+            Result = new { prices = prices }
+        });
+    }
 }
