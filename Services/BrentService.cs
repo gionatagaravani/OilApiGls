9afbbf29959082bfb132b69ddaeab6389ab4c7b7
Gionata Garavani <gionatagaravani@libero.it>
2025-11-03 22:19:15 +0100

    feat: brent service and dataurl appsettings

---
+++ Services/BrentService.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using OilTrendApi.Models;
+
+namespace OilTrendApi.Services;
+
+public class BrentService: IBrentService
+{
+    private readonly List<BrentRecord> pricesRecords;
+
+    public BrentService(Iconfiguration config)
+    {
+        // read appsettings data url
+        var dataUrl = config.GetValue<string>("DataUrl") ?? string.Empty;
+        
+        string json;
+
+        if (!string.IsNullOrEmpty(dataUrl)) {
+            try
+            {
+                using var client = new System.Net.Http.HttpClient();
+                json = client.GetStringAsync(dataUrl).GetAwaiter().GetResult();
+            }
+            catch
+            {
+                // get local data if url is invalid
+                json = File.ReadAllText("Data/brent-daily.json");
+            }
+        } else {
+            // get local data if url is null or empty
+            json = File.ReadAllText("Data/brent-daily.json");
+        }
+
+        pricesRecords = = JsonSerializer.Deserialize<List<BrentRecord>>(json);
+
+    }
+}
